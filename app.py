246817764c98fccad76d199391643dd30bddb544
Fanto974 <mathis.buclon@efrei.net>
2026-06-05 20:19:28 +0200

    add some things

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ import pyxel
 
 class Main:
     def __init__(self):
-        self.lvl = 1
+        # Pyxel
         pyxel.init(128,128,title="Tower Defence, protect your gold")
+        pyxel.load("U4.pyxres")
+
+        # Jeu
+        self.lvl = 1
+        self.gold = 0
+
+        # Obj
         self.castle = Castle(self.lvl,0)
         self.cave = Castle(self.lvl, 128-16)
         self.objToDraw = [self.castle, self.cave]
+
+        # Entity
         self.enemy_list = []
+
+        # Run
         pyxel.run(self.update, self.draw)
 
     def enemyCreation(self):
@@ -29,7 +40,12 @@ class Main:
         for obj in self.objToDraw:
             pyxel.rect(obj.x, obj.y, obj.height, obj.width, 9)
         for enemy in self.enemy_list:
-            pyxel.rect(enemy.x, enemy.y, enemy.height, enemy.width, 9)
+            if pyxel.frame_count % enemy.time_ani == 0:
+                enemy.animation = 8
+            else:
+                enemy.animation = 0
+            print(enemy.x, enemy.y, enemy.image, enemy.props[self.lvl-1][0] + enemy.animation, enemy.props[self.lvl-1][1], enemy.height, enemy.width, 15)
+            pyxel.blt(enemy.x, enemy.y, enemy.image, enemy.props[self.lvl-1][0] + enemy.animation, enemy.props[self.lvl-1][1], 8, 8, 15, 0, enemy.scale)
 
 
 
@@ -39,13 +55,18 @@ class Entity:
         self.damage = 5 * lvl
         self.x = x
         self.y = y
-        self.height = 4
-        self.width = 4
+        self.height = 8
+        self.width = 8
 
 class Enemy(Entity):
     def __init__(self, lvl, x, y):
         super().__init__(lvl, x, y)
+        self.scale = 1
         self.loot = 1 * lvl
+        self.image = 0
+        self.props = [(0, 64), (0, 72), (16, 64), (16, 72), (32, 64), (32, 72)]
+        self.animation = 0
+        self.time_ani = random.randint(1, 30)
 
     def move(self):
         if (random.randint(0, 100) < 20):
