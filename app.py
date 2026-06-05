eec35d9401373c68c0ec1334afc3134463209dba
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2026-06-05 20:54:13 +0200

    dzq

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Main:
 
         # Jeu
         self.lvl = 1
-        self.gold = 0
+        self.gold = 100
 
         # Obj
         self.castle = Castle(self.lvl,0)
@@ -20,20 +20,24 @@ class Main:
 
         # Entity
         self.enemy_list = []
+        self.tower_list = []
+        self.bullet_list = []
 
         # Run
         pyxel.run(self.update, self.draw)
 
     def enemyCreation(self):
-        if pyxel.frame_count % 30 == 0:
+        if pyxel.frame_count % 60 == 0:
             self.enemy_list.append(Enemy(1, self.cave.x, self.cave.y))
-            print("isbgfr")
+
 
     def update(self):
         self.enemyCreation()
         for enemy in self.enemy_list:
             if enemy.move() == False:
                 self.enemy_list.remove(enemy)
+        for tower in self.tower_list:
+            self.bullet_list.append(tower.shoot())
 
     def draw(self):
         pyxel.cls(0)
@@ -61,8 +65,13 @@ class Entity:
 class Enemy(Entity):
     def __init__(self, lvl, x, y):
         super().__init__(lvl, x, y)
+        self.scale = 1
         self.loot = 1 * lvl
-        right_or_left = random.randint(1,4)
+        self.image = 0
+        self.props = [(0, 64), (0, 72), (16, 64), (16, 72), (32, 64), (32, 72)]
+        self.animation = 0
+        self.time_ani = random.randint(1, 30)
+        right_or_left = random.randint(1, 4)
         if right_or_left == 1:
             self.direction = 80
         elif right_or_left == 2:
@@ -72,15 +81,14 @@ class Enemy(Entity):
         else:
             self.direction = 20
 
-
     def move(self):
         if pyxel.frame_count % 5 == 0:
             if (random.randint(0, 100) < 65):
                 deplacement = random.randint(1, 3)
                 if (self.x + 8 + deplacement > 128):
-                    self.x -= (deplacement+10)
+                    self.x -= (deplacement + 10)
                 elif (self.x - deplacement < 0):
-                    self.x += (deplacement+10)
+                    self.x += (deplacement + 10)
                 elif (random.randint(0, 100) < self.direction):
                     self.x += deplacement
                 else:
@@ -102,7 +110,24 @@ class Castle(Entity):
         self.height = 16
         self.width = 16
 
+class Tower:
+    def __init__(self, lvl, damages, prop, proj):
+        self.lvl = lvl
+        self.damage = damages * lvl
+        self.prop = prop
+        self.proj = proj
+        self.x = 0
+        self.y = 0
 
+    def shoot(self):
+        if pyxel.frame_count % 30 == 0:
+            return Proj(self.x, self.y, self.damage)
+
+class Proj:
+    def __init__(self, x, y, damage):
+        self.x = x
+        self.y = y
+        self.damage = damage
 
 
 Main()
