453bcb3af65424df063ae3a71f352ca4d2f01658
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2026-06-05 19:17:34 +0200

    base classes

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
+from ctypes import cast
+
 import pyxel
 
 class Main:
     def __init__(self):
         pyxel.init(128,128,title="Tower Defence, protect your gold")
-        
-        self.x = 0
+        self.castle = Castle(100, 0,64,0, 8, 8)
+        self.cave = Castle(100, 0, 64, 128-8, 8, 8)
+        self.objToDraw = [self.castle, self.cave]
         pyxel.run(self.update, self.draw)
         
 
     def update(self):
-        self.x = (self.x +1)%pyxel.width
+        pass
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        for obj in self.objToDraw:
+            pyxel.rect(obj.x, obj.y, 8, 8, 9)
+
+class Entity:
+    def __init__(self, pv, damage, x, y, height, width):
+        self.pv = pv
+        self.damage = damage
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
+class Enemy(Entity):
+    def __init__(self, pv, damage, x, y, height, width):
+        super().__init__(pv, damage, x, y, height, width)
+
+class Ally(Entity):
+    def __init__(self, pv, damage, x, y, height, width):
+        super().__init__(pv, damage, x, y, height, width)
+
+class Castle(Entity):
+    def __init__(self, pv, damage, x, y, height, width):
+        super().__init__(pv, damage, x, y, height, width)
+
+
+
 
 Main()
+
+
