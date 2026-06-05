6c54076b1d8ad9bb19e7d1ac29729650e1cbadd6
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2026-06-05 18:56:01 +0200

    Adding main app.py files
    Because it's good

---
+++ app.py
@@ -0,0 +1,18 @@
+import pyxel
+
+class Main:
+    def __init__(self):
+        pyxel.init(128,128,title="Tower Defence, protect your gold")
+        
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+        
+
+    def update(self):
+        self.x = (self.x +1)%pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+Main()
