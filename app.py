43e0824ffb07d5f59399c9c6e6bd829643e6b28b
Fanto974 <mathis.buclon@efrei.net>
2026-06-05 19:31:40 +0200

    add spawn

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ class Main:
         self.castle = Castle(100, 0,64,0, 8, 8)
         self.cave = Castle(100, 0, 64, 128-8, 8, 8)
         self.objToDraw = [self.castle, self.cave]
+        self.enemy_list = []
         pyxel.run(self.update, self.draw)
-        
+
+    def enemyCreation(self):
+        if (pyxel.frame_count % 30 == 0):
+            self.enemy_list.append(Enemy(25, 10, self.cave.x, self.cave.y, 8, 8))
+
 
     def update(self):
         pass
@@ -17,7 +22,11 @@ class Main:
     def draw(self):
         pyxel.cls(0)
         for obj in self.objToDraw:
-            pyxel.rect(obj.x, obj.y, 8, 8, 9)
+            pyxel.rect(obj.x, obj.y, obj.height, obj.width, 9)
+        for enemy in self.enemy_list:
+            pyxel.rect(enemy.x, enemy.y, enemy.height, enemy.width, 9)
+
+
 
 class Entity:
     def __init__(self, pv, damage, x, y, height, width):
@@ -31,6 +40,7 @@ class Entity:
 class Enemy(Entity):
     def __init__(self, pv, damage, x, y, height, width):
         super().__init__(pv, damage, x, y, height, width)
+
 
 class Ally(Entity):
     def __init__(self, pv, damage, x, y, height, width):
